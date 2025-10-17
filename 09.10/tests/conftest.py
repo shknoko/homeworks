de075289a806a47d6fd2e346b2808d06ddd85e75
Nikolay Baboshin <231540704+shknoko@users.noreply.github.com>
2025-10-18 00:23:16 +0300

    Fixed codestyle

--- 09.10/tests/conftest.py
+++ 09.10/tests/conftest.py
@@ -6,5 +6,5 @@ import pytest
 @pytest.fixture
 def random_list():
     list_len = random.randint(100, 5000)
-    random_list = [random.randint(-10**11, 10**11) for _ in range(list_len)]
+    random_list = [random.randint(-(10**11), 10**11) for _ in range(list_len)]
     return random_list
